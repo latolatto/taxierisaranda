09fe46042af46d17756568b7d9dc258b4449dba3
Laz <latolatto16@gmail.com>
2026-03-26 05:42:36 +0100

    reviews section transformation

--- js/script.js
+++ js/script.js
@@ -105,11 +105,25 @@ autoScroll();
  */
 
 const reviews = [
-    { name: "Elena R.", loc: "Italy", text: "Best taxi in Saranda! Spotless car and very professional." },
-    { name: "Mark T.", loc: "UK", text: "Airport transfer was perfect. On time and safe driving." },
-    { name: "Lukas M.", loc: "Germany", text: "Easier than the bus. Great price for Ksamil trips!" },
-    { name: "Sarah J.", loc: "USA", text: "Eri is the best. He knows all the hidden gems." },
-    { name: "David N.", loc: "Poland", text: "WhatsApp booking was so fast. Highly recommend!" }
+    { name: "Milka K.", loc: "Rwanda", text: "We found we put a wrong location. But he was very understanding, helpful and a good driver. Thank you Eri." },
+    { name: "Charlene M.", loc: "Zimbabwe", text: "Driver picked us from our hotel in Sarande to Tirana. He was very professional and we were so comfortable in there. Took a picture of the views on our way. Air conditioning was good . Highly recommend" },
+    { name: "Jess R.", loc: "UK", text: "Couldn't ask for better service. Excellent driver, reliable, fully functioning luxury car and even opens the door. Would recommend to all" },
+    { name: "HB McKenna", loc: "Ireland", text: "Excellent service. On time. Good driver. We will use again for future trips. Strongly recommend." },
+    { name: "Alex", loc: "USA", text: "Quick to respond. Very nice. And no problem finding pickup and drop off locations. This is my go to taxi service for the area." },
+        { name: "Christian T.", loc: "Ivory Coast", text: "Very good driver and polite too . Will recommend him to anyone who need taxi in Albania" },
+    { name: "Antje B.", loc: "Germany", text: "Great experience. The driver was friendly, courteous and accommodating." },
+    { name: "Stefan H.", loc: "Sweden", text: "On time, friendly and very service minded. I recommend Eri taxi if you want a fast and safe trip around Saranda and more!" },
+    { name: "Jaypee P.", loc: "Philippines", text: "The taxi is clean and the driver is very helpful. I'd recommended." },
+    { name: "Tarek K.", loc: "Lebanon", text: "Great driver, very professional and nice to talk to, functional AC. Recommend." },
+    { name: "Josilene B.", loc: "Brazil", text: "Very helpful when we needed to stop on the way.Talkative and sympathetic!5 stars ." },
+    { name: "Dimitra P.", loc: "Greece", text: "We had a great experience. Eri is very kind and friendly. He was on time and showed us around Sarande. His taxi service is highly recommended!" },
+    { name: "Nirujan K.", loc: "Sri Lanka", text: "A great taxi service! The taxi arrived quickly and right on time. The driver, Ervin T, was excellent, very polite, professional, and friendly. We felt safe and comfortable throughout the ride. Highly recommended!" },
+    { name: "Lola D.", loc: "Albania", text: "I traveled with this taxi and it left me with good impressions.The best" },
+    { name: "Caroline L.", loc: "Ireland", text: "Our driver who we used on 4 occasions was outstanding. He was prompt, polite, helpful & professional. Would highly recommend." },
+    { name: "Michelle L.", loc: "Brazil", text: "Great service, from helping with our bags, to getting us water and giving recommendations. 10/10" },
+    { name: "Daisy H.", loc: "UK", text: "best taxi driver, lovely service to Ksmail. Very useful would recommend." },
+    { name: "Ivy W.", loc: "China", text: "Eri was super friendly and chill! On time, safe driver. Highly recommend if you need a taxi around Sarandë!" }
+
 ];
 
 const track = document.getElementById('reviews-track');
@@ -118,7 +132,7 @@ const nextBtn = document.getElementById('nextBtn');
 const prevBtn = document.getElementById('prevBtn');
 
 // 1. INJECT 6 SETS FOR INFINITE BUFFER
-const bufferSet = [...reviews, ...reviews, ...reviews, ...reviews, ...reviews, ...reviews];
+const bufferSet = [...reviews, ...reviews, ...reviews, ...reviews, ...reviews, ...reviews, ...reviews, ...reviews, ...reviews, ...reviews, ...reviews, ...reviews, ...reviews, ...reviews, ...reviews, ...reviews, ...reviews, ...reviews, ...reviews];
 
 bufferSet.forEach((r) => {
     const card = document.createElement('div');
